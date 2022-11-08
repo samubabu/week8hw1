540ca68b2668abf94403af3aa143680fba551444
Sam B <samb@Sams-MacBook-Pro.local>
2022-11-07 21:43:35 -0600

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [list, setList] = useState([]);
+
+    const [input, setInput] = useState("");
+
+    
+    const addTodo = (todo) => {
+        const newToDo = {
+            id: Math.random(),
+            todo: todo
+        }
+//add the todo to the list
+setList([...list, newToDo]);
+
+//clear input box
+setInput("");
+    }
+
+const deleteTodo =(id) => {
+// filter out todo with the id
+
+const newList =list.filter((todo) => todo.id !==id);
+
+setList(newList);
+
+};
+    return(
+        <div className="centered-div">
+         <h1>To-do List  </h1>  
+         <input type="text" value ={input} 
+         onChange = {(e) => setInput(e.target.value)}/> 
+        <button onClick={()=>addTodo(input)}>Add</button>
+        <ul>
+            {list.map((todo) => (
+                <li key={todo.id}>
+                    {todo.todo}
+                    <button onClick={() => deleteTodo(todo.id)}>&times;</button>
+                </li>
+            ))}
+        </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
